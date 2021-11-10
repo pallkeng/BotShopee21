72257d77f71e8509d22de78b1429a10b851ef123
pallkeng <50643866+pallkeng@users.noreply.github.com>
2021-11-11 06:27:33 +0700

    Update bot.py

--- flasher/bot.py
+++ flasher/bot.py
@@ -43,7 +43,7 @@ class ShopeeBot:
             "referer": _urls.MALL_PREFIX,
             "if-none-match-": "*",
             "x-csrftoken": self.session.cookies.get("csrftoken"),
-            "user-agent": useragent.WEB
+            "user-agent": useragent.ANDROIDAPP
         })
 
     def set_user_agent(self, ua: str):
