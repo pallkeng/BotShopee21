b252d800ed9a6d94d60150c5b9bad0c23f5293c8
pallkeng <50643866+pallkeng@users.noreply.github.com>
2021-11-11 07:54:18 +0700

    Update useragent.py

--- flasher/constant/useragent.py
+++ flasher/constant/useragent.py
@@ -1,2 +1,3 @@
-WEB = "Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Build/QKQ1.200114.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.85 Mobile Safari/537.36"
+WEB = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"
 ANDROIDAPP = "Android app Shopee appver\u003d26921 app_type\u003d1"
+
